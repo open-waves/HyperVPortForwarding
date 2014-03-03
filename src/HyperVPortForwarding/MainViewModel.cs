35d60f06f5b1119d192e7eca4ea592a8c1e102eb
KMorcinek <KMorcinek@b0cb2f60-a98f-4b4f-b612-212f15a34d4a>
2014-03-03 09:47:38 +0000

    Binded to MVVM pattern. Code cleanup.
    git-svn-id: svn://making-svn/Making%20Waves/MakingWaves.Tools.HyperVPortForwarding/trunk@147431 b0cb2f60-a98f-4b4f-b612-212f15a34d4a

--- src/HyperVPortForwarding/MainViewModel.cs
+++ src/HyperVPortForwarding/MainViewModel.cs
@@ -1,7 +1,128 @@
-﻿namespace MakingWaves.Tools.HyperVPortForwarding
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace MakingWaves.Tools.HyperVPortForwarding
 {
     public class MainViewModel : ViewModelBase
     {
-         
+        private readonly Dispatcher _dispatcher;
+
+        public MainViewModel()
+        {
+            Items = new ObservableCollection<ForwardedPort>();
+
+            _dispatcher = Dispatcher.CurrentDispatcher;
+            RefreshConnections();
+        }
+
+        public ObservableCollection<ForwardedPort> Items { get; set; }
+
+        private ForwardedPort _selectedForwardedPort;
+        public ForwardedPort SelectedForwardedPort
+        {
+            get { return _selectedForwardedPort; }
+            set { SetField(ref _selectedForwardedPort, value, () => SelectedForwardedPort); }
+        }
+
+        RelayCommand _addPortCommand;
+        public ICommand AddPortCommand
+        {
+            get
+            {
+                return _addPortCommand ?? (_addPortCommand = new RelayCommand(param => OnAddPortCommand(), param => true));
+            }
+        }
+
+        private void OnAddPortCommand()
+        {
+            // TODO validation
+//                        if (String.IsNullOrEmpty(AddListenPort.Text) || String.IsNullOrEmpty(AddConnectAddress.Text) ||
+//                            String.IsNullOrEmpty(AddConnectPort.Text) || int.Parse(AddListenPort.Text) == 0 || int.Parse(AddConnectPort.Text) == 0)
+//                        {
+//                            // validation
+//                            return;
+//                            ValidationLabel.Content = String.Format("Please set {0}, {1} and {2}", LocalPort, VmAddress, VmPort);
+//                            ValidationLabel.Visibility = Visibility.Visible;
+//                            SelectErrorTextBox(AddConnectAddress);
+//                            SelectErrorTextBox(AddListenPort);
+//                            SelectErrorTextBox(AddConnectPort);
+//                        }
+//                        else
+            {
+                // TODO todo
+                RunReadProcess(String.Format("netsh interface portproxy add v4tov4 {0} {1} {2}", 8003, "192.168.2.2", 8003));
+
+                RefreshConnections();
+            }
+        }
+
+        public void RefreshConnections()
+        {
+            Items.Clear();
+            RunProcess("netsh interface portproxy show all");
+        }
+
+        RelayCommand _removePortCommand;
+        public ICommand RemovePortCommand
+        {
+            get
+            {
+                return _removePortCommand ?? (_removePortCommand = new RelayCommand(param => OnRemovePortCommand(), param => true));
+            }
+        }
+
+        private void OnRemovePortCommand()
+        {
+            RunReadProcess("netsh interface portproxy delete v4tov4 " + SelectedForwardedPort.HostPort); // TODO not sure
+            RefreshConnections();
+        }
+
+        private void RunProcess(string args)
+        {
+            var process = CreateProcess(args);
+            process.OutputDataReceived += process_OutputDataReceived;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+        }
+
+        private void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(e.Data) || e.Data.StartsWith("*") == false)
+                return;
+
+            var forwardedPort = new ForwardedPortFactory().Create(e.Data);
+
+            Action del = () => Items.Add(forwardedPort);
+            _dispatcher.BeginInvoke(del);
+        }
+
+        private static void RunReadProcess(string args)
+        {
+            var process = CreateProcess(args);
+            process.Start();
+            process.WaitForExit();
+        }
+
+        private static Process CreateProcess(string args)
+        {
+            var processStartInfo = new ProcessStartInfo("cmd.exe")
+            {
+                Arguments = "/c " + args,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = new Process
+            {
+                StartInfo = processStartInfo
+            };
+
+            return process;
+        }
     }
 }
--- src/HyperVPortForwarding/MainWindow.xaml.cs
+++ src/HyperVPortForwarding/MainWindow.xaml.cs
@@ -1,185 +1,29 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace MakingWaves.Tools.HyperVPortForwarding
 {
     public partial class MainWindow
     {
-        private const string LocalPort = "Local port";
-        private const string VmAddress = "VM address";
-        private const string VmPort = "VM port";
-
-        private ObservableCollection<String> _items;
-        public ObservableCollection<String> Items
-        {
-            get { return _items ?? (_items = new ObservableCollection<String>()); }
-        }
-
         public MainWindow()
         {
             InitializeComponent();
-            PortsList.ItemsSource = Items;
-
-            RefreshConnections();
         }
 
-        private void RefreshConnections()
+        private void PreviewTextInputForPortNumber(object sender, TextCompositionEventArgs e)
         {
-            //            if (sender != null && e != null)
-            ClearInputs();
-
-            _items.Clear();
-            RunProcess("netsh interface portproxy show all");
+            var regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
 
-        private void Delete_OnClick(object sender, RoutedEventArgs e)
+        private void PreviewTextInputForIpAddress(object sender, TextCompositionEventArgs e)
         {
-            ClearInputs();
-
-            if (WithValidation)
-            {
-                ValidationLabel.Visibility = Visibility.Visible;
-                ValidationLabel.Content = "Possible outcome:\n" +
-                                            "Blank window - deletion of port forwarding completed successfully\n" +
-                                            "'The system cannot find the file specified.' - no existing forwarding\n" +
-                                            "'The requested operation requires elevation (Run as administrator)' - run program\nas administrator";
-            }
-
-            var splitted = PortsList.SelectedValue.ToString().Split(',')[0].Split(' ');
-            var deletedListenPort = splitted[splitted.Count() - 1];
-            if (!String.IsNullOrEmpty(deletedListenPort))
-            {
-                RunReadProcess("netsh interface portproxy delete v4tov4 " + deletedListenPort);
-                RefreshConnections();
-            }
-            else
-            {
-                ValidationLabel.Content = LocalPort + " is empty!";
-            }
-        }
-
-        private void Add_OnClick(object sender, RoutedEventArgs e)
-        {
-            ClearInputs();
-            if (String.IsNullOrEmpty(AddListenPort.Text) || String.IsNullOrEmpty(AddConnectAddress.Text) ||
-                String.IsNullOrEmpty(AddConnectPort.Text) || int.Parse(AddListenPort.Text) == 0 || int.Parse(AddConnectPort.Text) == 0)
-            {
-                // validation
-                return;
-                ValidationLabel.Content = String.Format("Please set {0}, {1} and {2}", LocalPort, VmAddress, VmPort);
-                ValidationLabel.Visibility = Visibility.Visible;
-                SelectErrorTextBox(AddConnectAddress);
-                SelectErrorTextBox(AddListenPort);
-                SelectErrorTextBox(AddConnectPort);
-            }
-            else
-            {
-                if (WithValidation)
-                {
-                    ValidationLabel.Visibility = Visibility.Visible;
-                    ValidationLabel.Content = "Desire outcome:\n" +
-                                              "Blank window - port forwarding completed successfully\n" +
-                                              "'The requested operation requires elevation (Run as administrator)' - run program\nas administrator";
-
-                }
-
-                RunReadProcess(String.Format("netsh interface portproxy add v4tov4 {0} {1} {2}", AddListenPort.Text, AddConnectAddress.Text, AddConnectPort.Text));
-                RefreshConnections();
-            }
-        }
-
-        public bool WithValidation = false;
-
-        private void TextBox_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            var tb = (TextBox)sender;
-            if (tb == null)
-                return;
-            int i;
-            if (!int.TryParse(tb.Text, out i))
-                tb.Text = i.ToString(CultureInfo.InvariantCulture);
-        }
-
-        private void SelectErrorTextBox(TextBox textBox)
-        {
-            textBox.BorderThickness = new Thickness(3.0);
-            textBox.BorderBrush = Brushes.Red;
-        }
-
-        private void ClearInputs()
-        {
-            return;
-            ValidationLabel.Visibility = Visibility.Hidden;
-            AddConnectAddress.BorderThickness = new Thickness(1.0);
-            AddConnectAddress.BorderBrush = Brushes.DarkGray;
-            AddListenPort.BorderThickness = new Thickness(1.0);
-            AddListenPort.BorderBrush = Brushes.DarkGray;
-            AddConnectPort.BorderThickness = new Thickness(1.0);
-            AddConnectPort.BorderBrush = Brushes.DarkGray;
-        }
-
-        private void RunProcess(string args)
-        {
-            var proc = CreateProcess(args);
-            proc.OutputDataReceived += process_OutputDataReceived;
-            proc.Start();
-            proc.BeginOutputReadLine();
-            proc.WaitForExit();
-        }
-        private void RunReadProcess(string args)
-        {
-            var proc = CreateProcess(args);
-            proc.Start();
-
-            var outputReader = proc.StandardOutput;
-            proc.WaitForExit();
-
-            string displayText = "Output" + Environment.NewLine + "==============" + Environment.NewLine;
-            displayText += outputReader.ReadToEnd();
-            ConsoleBox.Text += displayText;
-        }
-
-        private static Process CreateProcess(string args)
-        {
-            var processStartInfo = ProcessStartInfo(args);
-            var proc = new Process();
-            proc.StartInfo = processStartInfo;
-            return proc;
-        }
-
-        private static ProcessStartInfo ProcessStartInfo(string args)
-        {
-            var processStartInfo = new ProcessStartInfo("cmd.exe")
-            {
-                Arguments = "/c " + args
-            };
-            processStartInfo.RedirectStandardOutput = true;
-            processStartInfo.UseShellExecute = false;
-            processStartInfo.CreateNoWindow = true;
-            processStartInfo.UseShellExecute = false;
-            return processStartInfo;
-        }
-
-        private void process_OutputDataReceived(object sender, DataReceivedEventArgs e)
-        {
-            if (String.IsNullOrEmpty(e.Data)) return;
-            if (e.Data.StartsWith("*"))
-            {
-                var forwardedPort = new ForwardedPortFactory().Create(e.Data);
-
-                var str = String.Format("{0}: {1}, {2}: {3}, {4}: {5}",
-                    LocalPort, forwardedPort.HostPort,
-                    VmAddress, forwardedPort.VmIpAddress,
-                    VmPort, forwardedPort.VmPort);
-
-                Dispatcher.BeginInvoke(new Action(() => _items.Add(str)));
-            }
+            var regex = new Regex("[^0-9.]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
